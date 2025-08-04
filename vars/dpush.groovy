64fdda802e36ac79d02955aa5d7c84259728ccce
AdityaGaikwad6430 <adityagaikwad6430@gmail.com>
2025-08-04 14:50:10 +0530

    Update dpush.groovy

--- vars/dpush.groovy
+++ vars/dpush.groovy
@@ -1,6 +1,6 @@
-def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'dockerHubcreds', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
-      sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
-  }
-  sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
+def call(String project, String imageTag, String credsId) {
+    withCredentials([usernamePassword(credentialsId: credsId, passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
+        sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+        sh "docker push ${dockerHubUser}/${project}:${imageTag}"
+    }
 }
