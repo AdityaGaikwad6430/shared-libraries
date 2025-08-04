0c4833ac49f8d92a9dfae065e73b0b0a6e7b1cc7
AdityaGaikwad6430 <adityagaikwad6430@gmail.com>
2025-08-04 14:25:09 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,4 +1,4 @@
-def call(String ProjectName, String ImageTag, String DockerHubUser){
+def call(String ProjectName, String ImageTag){
   withCredentials([usernamePassword(credentialsId: 'dockerHubcreds', usernameVariable: 'dockerHubUser')]) {
   }
   sh "sudo docker build -t ${dockerHubUser}/${ProjectName}:${ImageTag} ."
