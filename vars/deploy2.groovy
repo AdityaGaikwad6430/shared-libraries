cb83f4ec3f1bca23cbff0fd235379e5e5db4f2c3
AdityaGaikwad6430 <adityagaikwad6430@gmail.com>
2025-11-03 13:04:58 +0530

    Update deploy2.groovy

--- vars/deploy2.groovy
+++ vars/deploy2.groovy
@@ -1,5 +1,5 @@
 def call(){
-  sh "sudo docker-compose down"
-  sh"sudo docker-compose up -d --build"
+  sh " docker-compose down"
+  sh" docker-compose up -d --build"
   echo "Deployment successful"
 }
